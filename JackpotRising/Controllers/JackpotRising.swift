481414f121c90b8a8e78b49c6c12f4deeede9688
Prethush <prethushpj@gmail.com>
2017-07-06 16:47:42 +0530

    public api setup

--- JackpotRising/Controllers/JackpotRising.swift
+++ JackpotRising/Controllers/JackpotRising.swift
@@ -11,5 +11,33 @@ import JackpotRising.Private
 
 public class JackpotRising: NSObject {
     
-    //comment
+    public static let sharedInstance = JackpotRising()
+    internal var appViewController:UIViewController{
+        get{
+            return UIApplication.shared.keyWindow!.rootViewController!
+        }
+    }
+    internal var sdkRootViewController: JRContainerViewController{
+        get{
+            let storyboard = UIStoryboard (
+                name: "JRMain",
+                bundle: Bundle(for: JRContainerViewController.self))
+            
+            return storyboard.instantiateInitialViewController()
+        }
+    }
+    
+    private override init() {
+        super.init()
+        CustomFont.load("OpenSans-Regular")
+        CustomFont.load("OpenSans-Bold")
+        CustomFont.load("OpenSans-Light")
+        CustomFont.load("OpenSans-ExtraBold")
+    }
+}
+
+extension JackpotRising{
+    public func showSDK(){
+        
+    }
 }
